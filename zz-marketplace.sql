1b38e62c4665a45d2f17457b36d07e3cefe7c8cf
Zartoz <84977778+Zartoz@users.noreply.github.com>
2024-08-12 17:12:01 +0300

    Update zz-marketplace.sql

--- zz-marketplace.sql
+++ zz-marketplace.sql
@@ -1,22 +1,20 @@
 CREATE TABLE IF NOT EXISTS `marketplace_items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `seller_id` varchar(50) NOT NULL,
+  `seller_name` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `label` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  `seller` varchar(255) NOT NULL,
-  `description` text DEFAULT NULL,
-  `seller_id` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `is_sold` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
-CREATE TABLE IF NOT EXISTS `marketplace_recent_buys` (
+CREATE TABLE IF NOT EXISTS `recent_buys` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `item_label` varchar(255) NOT NULL,
-  `price` int(11) NOT NULL,
-  `buyer` varchar(255) NOT NULL,
-  `seller` varchar(255) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  `item_name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `buyer_name` varchar(255) NOT NULL,
+  `seller_name` varchar(255) NOT NULL,
+  `bought_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
