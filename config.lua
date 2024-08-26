23fca30f3eda5d6c72eae33050d7dc9c7a7c82fb
Zartoz <84977778+Zartoz@users.noreply.github.com>
2024-08-27 01:24:18 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,5 +6,15 @@ Config.MarketplaceCoords = vector3(452.6795, -783.4480, 27.3578)
 -- Choose the targeting system: 'ox_target' or 'qb-target'
 Config.TargetSystem = 'qb-target'
 
+-- Ped Config
+Config.PedCoords = vector4(454.36, -782.77, 26.36, 273.85)
+Config.PedModel = `a_m_y_stbla_02`
+
+-- Blacklisted Items
+Config.BlacklistedItems = {
+    "weapon_pistol", 
+    "water_bottle"
+}
+
 -- Maximum number of recent buys to store
 Config.MaxRecentBuys = 10
